40f0f9ca89adecac54c752cf3751e5625024dc3e
Adam Wei <adam.wei@mail.utoronto.ca>
2021-08-22 11:13:37 -0400

    moved event input and debug output to python

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import serial
 
 def main():
     ser = serial.Serial(port='COM3', baudrate=9600)
-    start = time.time()
     while (True):
         data = ser.readline().decode()
-        if data[0] == 'M':
+        print(data)
+        if data.startswith("Enter"): # enter event
+            event = input()
+            if event == "exit":
+                return
+            ser.write(event.encode())
+        elif data[0] == 'M':
             move = data[3:]
-            print(move)
-        if time.time() - start > 10:
-            return
+            print("Received: ", move)
 
 if __name__ == "__main__":
     main()
